dfac4e82d3ffde5e8491f49d1d8bcee263a9dba4
n1lsqn <owner@n1l.dev>
2024-12-08 07:17:42 +0000

    feat: ざっくりレスポンシブ対応

--- src/features/leftSide.tsx
+++ src/features/leftSide.tsx
@@ -6,42 +6,42 @@ import { SiVrchat } from 'react-icons/si';
 
 export const LeftSide: NextPage = () => {
   return (
-    <div className='bg-white rounded-l-xl w-1/2'>
+    <div className='bg-white rounded-t-xl md:rounded-l-xl md:rounded-r-none w-full md:w-1/2'>
       <div className='flex justify-center'>
         <Image
           src="/assets/banner.png"
           alt='banner'
           width={0}
           height={0}
-          className='rounded-tl-xl'
+          className='rounded-t-xl md:rounded-tl-xl'
           sizes="100vw"
           style={{ width: '100%', height: 'auto' }}
         />
       </div>
-      <div className='flex justify-center -mt-24'>
+      <div className='flex justify-center -mt-12 md:-mt-24'>
         <Image
           src="/assets/avatar.jpg"
           alt='avatar img'
           width={0}
           height={0}
           sizes='100vw'
-          style={{ width: '50%', height: 'auto' }}
-          className='rounded-full'
+          style={{ width: '50%', maxWidth: '200px', height: 'auto' }}
+          className='rounded-full border-4 border-white'
         />
       </div>
       <div className='text-xl text-center my-2'>にる</div>
-      <div className='flex flex-row gap-4 justify-center mt-48 mb-16'>
-        <div className='rounded-full border p-2'>
-          <SiVrchat size="3.5vw"/>
+      <div className='flex flex-row gap-4 justify-center mt-8 md:mt-48 mb-8 md:mb-16 px-4'>
+        <div className='rounded-full border p-2 hover:bg-gray-100 transition-colors'>
+          <SiVrchat className='w-10 h-10 md:w-12 md:h-12'/>
         </div>
-        <div className='rounded-full border p-2'>
-          <CiTwitter size="3.5vw" />
+        <div className='rounded-full border p-2 hover:bg-gray-100 transition-colors'>
+          <CiTwitter className='w-10 h-10 md:w-12 md:h-12'/>
         </div>
-        <div className='rounded-full border p-2'>
-          <CiShoppingCart size="3.5vw" />
+        <div className='rounded-full border p-2 hover:bg-gray-100 transition-colors'>
+          <CiShoppingCart className='w-10 h-10 md:w-12 md:h-12'/>
         </div>
-        <div className='rounded-full border p-2'>
-          <FaAmazon size="3.5vw" />
+        <div className='rounded-full border p-2 hover:bg-gray-100 transition-colors'>
+          <FaAmazon className='w-10 h-10 md:w-12 md:h-12'/>
         </div>
       </div>
     </div>
--- src/features/rightSide.tsx
+++ src/features/rightSide.tsx
@@ -2,12 +2,22 @@ import { NextPage } from 'next';
 
 export const RightSide: NextPage = () => {
   return (
-    <div className='bg-white rounded-r-xl w-1/2'>
-      2024/9/11 Join, /13 NU, /19 U, 10/7 KU, 11/2 TU<br/>
-      ゆるーく遊んでます、チルワールド巡ったり、銃撃つのが好き。<br/>
-      だいたいV睡してます！書き置き来てくれたらうれしー！！<br/>
-      <br/>
-      アバターは<a href='https://nanka-tsukurouyo.booth.pm/items/5787485'>むんきちゃん</a>を使っています！！
+    <div className='bg-white rounded-b-xl md:rounded-r-xl md:rounded-l-none w-full md:w-1/2 p-4 text-sm md:text-base'>
+      <p className='mb-2'>2024/9/11 Join, /13 NU, /19 U, 10/7 KU, 11/2 TU</p>
+      <p className='mb-2'>ゆるーく遊んでます、チルワールド巡ったり、銃撃つのが好き。</p>
+      <p className='mb-2'>だいたいV睡してます！書き置き来てくれたらうれしー！！</p>
+      <p className='mt-4'>
+        アバターは
+        <a 
+          href='https://nanka-tsukurouyo.booth.pm/items/5787485' 
+          className='text-blue-600 hover:underline'
+          target='_blank' 
+          rel='noopener noreferrer'
+        >
+          むんきちゃん
+        </a>
+        を使っています！！
+      </p>
     </div>
   );
 };
