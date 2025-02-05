9c94f3783b348342510a95fc4e3e73ce7ff5d486
n1lsqn <owner@n1l.dev>
2025-02-05 01:51:50 +0000

    fix: ci通す

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-import { Darumadrop_One, Hachi_Maru_Pop, Monomaniac_One, Slackside_One, Yomogi, Yusei_Magic, Zen_Maru_Gothic } from 'next/font/google'
+import { Zen_Maru_Gothic } from 'next/font/google'
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const font = Zen_Maru_Gothic({
   weight: "400",
   subsets: ['latin']
 })
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Sea Slug",
