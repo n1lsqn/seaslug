a16fcc73ab07ed5fb74e3197c430716a1848286b
n1lsqn <owner@n1l.dev>
2025-02-09 17:02:02 +0900

    feat: bg-cover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { RightSide } from "@/features/rightSide";
 
 const Home = () => {
   return (
-    <div className="bg-[url('/assets/bg_blur.png')] flex flex-col justify-center items-center min-h-screen p-4 shadow-2xl">
+    <div className="bg-[url('/assets/bg_blur.png')] bg-cover flex flex-col justify-center items-center min-h-screen p-4 shadow-2xl">
       <div className="w-full max-w-4xl flex flex-col md:flex-row shadow-2xl">
         <LeftSide />
         <div className="border-t md:border-t-0 md:border-l"></div>
